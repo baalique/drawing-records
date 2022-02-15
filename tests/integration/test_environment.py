69d00b5d7c302d679421a294a79dd4223db4cfc3
baalique <baalique@gmail.com>
2022-02-15 11:51:49 +0700

    TESTS: settings

---
+++ tests/integration/test_environment.py
@@ -0,0 +1,18 @@
+import pytest
+
+from config import Settings, get_initial_app_settings, get_current_app_settings
+
+
+@pytest.mark.integration
+def test_settings():
+    assert Settings()
+
+
+@pytest.mark.integration
+def test_initial_settings():
+    assert get_initial_app_settings()
+
+
+@pytest.mark.integration
+def test_current_settings():
+    assert get_current_app_settings()
