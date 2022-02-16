2da6143ed10e56013a7745b830c02c00984d4057
baalique <baalique@gmail.com>
2022-02-16 15:51:34 +0700

    TESTS: refactor fixtures structure

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from pytest_factoryboy import register
+
+from tests.factories.entities.drawing import FactoryDrawing, FactoryDrawingCreate, FactoryDrawingUpdate
+from tests.utils import make_many
+
+for factory in (FactoryDrawing, FactoryDrawingCreate, FactoryDrawingUpdate):
+    register(factory)
+
+
+@pytest.fixture(name="drawing")
+def drawing(factory_drawing):
+    return factory_drawing()
+
+
+@pytest.fixture(name="drawings")
+def drawings(factory_drawing):
+    return lambda amount=10: make_many(factory_drawing, amount)
+
+
+@pytest.fixture(name="create_drawing_dto")
+def create_drawing_dto_fixture(factory_drawing_create):
+    return factory_drawing_create()
+
+
+@pytest.fixture(name="create_many_drawing_dto")
+def create_many_drawing_dto_fixture(factory_drawing_create):
+    return lambda amount=10: make_many(factory_drawing_create, amount)
+
+
+@pytest.fixture(name="update_drawing_dto")
+def update_drawing_dto_fixture(factory_drawing_update):
+    return factory_drawing_update()
