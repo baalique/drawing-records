51d95d9ba591d3fccaa621e2798383ea28829053
baalique <baalique@gmail.com>
2022-02-23 13:20:33 +0700

    TESTS: settings host and dsn

--- tests/integration/test_environment.py
+++ tests/integration/test_environment.py
@@ -16,3 +16,15 @@ def test_initial_settings():
 @pytest.mark.integration
 def test_current_settings():
     assert get_current_app_settings()
+
+
+@pytest.mark.integration
+def test_settings_host():
+    settings = Settings()
+    assert settings.host
+
+
+@pytest.mark.integration
+def test_settings_dsn():
+    settings = Settings()
+    assert settings.dsn
