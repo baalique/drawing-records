6d22805ec9a46ed0dd671cd34960c5f0fd2bb69a
baalique <baalique@gmail.com>
2022-03-01 17:48:40 +0700

    ADD: drawing crud services

---
+++ app/service_layer/services/drawing.py
@@ -0,0 +1,79 @@
+from typing import List, Optional
+
+from app.infrastructure.adapters.repositories.protocols.entities import (
+    DrawingRepository,
+)
+from app.service_layer.dtos.drawing import (
+    DrawingDtoCreate,
+    DrawingDtoOut,
+    DrawingDtoUpdate,
+)
+from app.service_layer.exceptions import AlreadyExistsError, NotFoundError
+
+
+async def create_drawing(
+    drawing_create: DrawingDtoCreate, repo: DrawingRepository
+) -> Optional[DrawingDtoOut]:
+    exists = await id_exists(drawing_create.id, repo)
+    if exists:
+        raise AlreadyExistsError(f"Drawing with id {drawing_create.id} already exists")
+
+    if drawing_create.parent_id is None:
+        parent = None
+    else:
+        parent = await repo.get(drawing_create.parent_id)
+        if not parent:
+            raise NotFoundError(f"Drawing with id {drawing_create.parent_id} not found")
+
+    drawing = drawing_create.to_entity(parent)
+    result = await repo.add(drawing)
+    if not result:
+        return
+    dto_out = DrawingDtoOut.from_entity(result)
+    return dto_out
+
+
+async def get_drawing_by_id(
+    id: int, repo: DrawingRepository
+) -> Optional[DrawingDtoOut]:
+    result = await repo.get(id)
+    if not result:
+        return
+    return DrawingDtoOut.from_entity(result)
+
+
+async def get_all_drawings(repo: DrawingRepository) -> List[DrawingDtoOut]:
+    drawings = await repo.list()
+    return list(map(DrawingDtoOut.from_entity, drawings))
+
+
+async def update_drawing(
+    id: int, drawing_update: DrawingDtoUpdate, repo: DrawingRepository
+) -> Optional[DrawingDtoOut]:
+    exists = await id_exists(drawing_update.id, repo)
+    if exists and id != drawing_update.id:
+        raise AlreadyExistsError(f"Drawing with id {drawing_update.id} already exists")
+    exists = await id_exists(drawing_update.parent_id, repo)
+    if drawing_update.parent_id is not None and not exists:
+        raise NotFoundError(f"Drawing with id {drawing_update.parent_id} not found")
+
+    drawing_to_update = await repo.get(id)
+    if not drawing_to_update:
+        return
+
+    drawing = await repo.update(drawing_update, id)
+    return DrawingDtoOut.from_entity(drawing)
+
+
+async def delete_drawing(id: int, repo: DrawingRepository) -> bool:
+    exists = await id_exists(id, repo)
+    if not exists:
+        raise NotFoundError(f"Drawing with id {id} not found")
+
+    result = await repo.delete(id)
+    return result
+
+
+async def id_exists(id: int, repo: DrawingRepository) -> bool:
+    all_drawings = await get_all_drawings(repo)
+    return id in (d.id for d in all_drawings)
