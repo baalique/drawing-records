2edd6f84a3c694bb112a21007039b3e04a6ce964
baalique <baalique@gmail.com>
2022-03-02 17:48:51 +0700

    ADD: refactor database initialization

--- app/infrastructure/db/db.py
+++ app/infrastructure/db/db.py
@@ -6,19 +6,23 @@ from sqlalchemy.orm import sessionmaker
 from app.config import get_current_app_settings
 from app.infrastructure.adapters.orm import start_mappers
 
-settings = get_current_app_settings()
 
-engine = create_async_engine(settings.dsn, pool_pre_ping=True)
+def init_db() -> sessionmaker:
+    settings = get_current_app_settings()
 
-session_factory = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    autocommit=False,
-    autoflush=False,
-    expire_on_commit=False,
-)
+    engine = create_async_engine(settings.dsn, pool_pre_ping=True)
+    start_mappers()
 
-start_mappers()
+    return sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        autocommit=False,
+        autoflush=False,
+        expire_on_commit=False,
+    )
+
+
+session_factory = init_db()
 
 
 async def get_db() -> AsyncGenerator:
