255575371e6f4b658763ac7a80003a4bc6f828c8
baalique <baalique@gmail.com>
2022-03-04 16:19:06 +0700

    TESTS: update test client fixtures

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -2,11 +2,14 @@ from typing import Dict, List
 
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from app.config import get_initial_app_settings
+from app.infrastructure.adapters.orm import metadata
+from app.infrastructure.db.db import get_db
 from app.initialization import init_app
 from app.service_layer.dtos import AbstractDtoOut
-from tests.utils import clear_database
+from tests.utils import clear_database, get_test_db
 
 
 @pytest.fixture(name="app_settings")
@@ -19,8 +22,21 @@ def app_fixture(app_settings):
     return init_app(app_settings)
 
 
+@pytest.fixture(name="recreate_db")
+async def _recreate_db_fixture():
+    engine = create_async_engine(
+        get_initial_app_settings().test_dsn, pool_pre_ping=True
+    )
+    metadata.bind = engine
+    async with engine.begin() as conn:
+        await conn.run_sync(metadata.drop_all)
+    async with engine.begin() as conn:
+        await conn.run_sync(metadata.create_all)
+
+
 @pytest.fixture(name="test_client")
-def test_client_fixture(app, app_settings):
+def test_client_fixture(app, app_settings, recreate_db):
+    app.app.dependency_overrides[get_db] = get_test_db
     with clear_database(app):
         client = TestClient(app.app)
         client.base_url += app_settings.API_PREFIX.rstrip("/") + "/"
--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,13 @@
-import asyncio
 from contextlib import contextmanager
 from typing import Any, Callable, List, Optional
 
 from factory.base import FactoryMetaClass
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
 
 from app.application import Application
+from app.config import get_initial_app_settings
+from app.infrastructure.adapters.orm import metadata
 
 
 def make_many(
@@ -29,7 +32,24 @@ def make_many(
 
 @contextmanager
 def clear_database(app: Application):
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(app.db.truncate_database())
     yield
-    loop.run_until_complete(app.db.truncate_database())
+
+
+async def _init_test_db(dsn: str) -> sessionmaker:
+    engine = create_async_engine(dsn, pool_pre_ping=True)
+    metadata.bind = engine
+    async with engine.begin() as conn:
+        await conn.run_sync(metadata.create_all)
+    return sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        autocommit=False,
+        autoflush=False,
+        expire_on_commit=False,
+    )
+
+
+async def get_test_db() -> AsyncSession:
+    session_factory = await _init_test_db(get_initial_app_settings().test_dsn)
+    async with session_factory() as session:
+        yield session
