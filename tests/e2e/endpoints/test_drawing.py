ced294ae44de4cc0ab0a636669d11575076d60bc
baalique <baalique@gmail.com>
2022-03-05 13:03:16 +0700

    TESTS: update drawing api tests

--- tests/e2e/endpoints/test_drawing.py
+++ tests/e2e/endpoints/test_drawing.py
@@ -4,15 +4,41 @@ from fastapi.encoders import jsonable_encoder
 
 
 @pytest.mark.e2e
-def test_create_success(test_client, create_drawing_dto):
+def test_create_success(test_client, drawing_dto_create):
     with test_client:
         response = test_client.post(
-            "drawing/new", json=jsonable_encoder(create_drawing_dto)
+            "drawing/new", json=jsonable_encoder(drawing_dto_create)
         )
         data = response.json()
 
         assert response.status_code == status.HTTP_201_CREATED
-        assert data["id"] == 1
+        assert data["id"] == drawing_dto_create.id
+
+
+@pytest.mark.e2e
+def test_create_fails_parent_not_found(test_client, drawing_dto_create):
+    with test_client:
+        drawing_dto_create.parent_id = drawing_dto_create.id + 1
+
+        response = test_client.post(
+            "drawing/new", json=jsonable_encoder(drawing_dto_create)
+        )
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.e2e
+def test_create_fails_already_exists(test_client, drawings_dto_create):
+    with test_client:
+        drawing_create_1, drawing_create_2 = drawings_dto_create(2)
+        test_client.post("drawing/new", json=jsonable_encoder(drawing_create_1))
+        drawing_create_2.id = drawing_create_1.id
+
+        response = test_client.post(
+            "drawing/new", json=jsonable_encoder(drawing_create_2)
+        )
+
+        assert response.status_code == status.HTTP_409_CONFLICT
 
 
 @pytest.mark.e2e
@@ -24,12 +50,13 @@ def test_create_fails_validation(test_client):
 
 
 @pytest.mark.e2e
-def test_get_one_success(test_client, create_drawing_dto):
+def test_get_one_success(test_client, drawing_dto_create):
     with test_client:
         create_response = test_client.post(
-            "drawing/new", json=jsonable_encoder(create_drawing_dto)
+            "drawing/new", json=jsonable_encoder(drawing_dto_create)
         )
         id_ = create_response.json()["id"]
+
         response = test_client.get(f"drawing/{id_}")
 
         assert response.status_code == status.HTTP_200_OK
@@ -53,11 +80,12 @@ def test_get_all_empty(test_client):
 
 
 @pytest.mark.e2e
-def test_get_all(test_client, create_many_drawings_dto):
-    drawings = create_many_drawings_dto()
+def test_get_all(test_client, drawings_dto_create):
+    drawings = drawings_dto_create()
     with test_client:
         for dto in drawings:
             test_client.post("drawing/new", json=jsonable_encoder(dto))
+
         response = test_client.get("drawing/all")
         data = response.json()
 
@@ -66,62 +94,106 @@ def test_get_all(test_client, create_many_drawings_dto):
 
 
 @pytest.mark.e2e
-def test_update_success(test_client, create_drawing_dto, update_drawing_dto):
+def test_update_success(test_client, drawing_dto_create, drawing_dto_update):
     with test_client:
         create_response = test_client.post(
-            "drawing/new", json=jsonable_encoder(create_drawing_dto)
+            "drawing/new", json=jsonable_encoder(drawing_dto_create)
         )
         id_ = create_response.json()["id"]
 
         response = test_client.patch(
-            f"drawing/{id_}", json=jsonable_encoder(update_drawing_dto)
+            f"drawing/{id_}", json=jsonable_encoder(drawing_dto_update)
         )
 
         assert response.status_code == status.HTTP_200_OK
         assert all(
-            response.json()[key] == update_drawing_dto.dict()[key]
+            response.json()[key] == drawing_dto_update.dict()[key]
             for key in ("name", "category", "project")
         )
 
 
 @pytest.mark.e2e
-def test_update_fails_not_found(test_client, update_drawing_dto):
+def test_update_fails_not_found(test_client, drawing_dto_update):
     with test_client:
         response = test_client.patch(
-            "drawing/1", json=jsonable_encoder(update_drawing_dto)
+            "drawing/1", json=jsonable_encoder(drawing_dto_update)
         )
 
         assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
-@pytest.mark.e2e
-def test_update_wrong_data_not_affect(
-    test_client, create_drawing_dto, update_drawing_dto
+def test_update_fails_parent_not_found(
+    test_client, drawing_dto_create, drawing_dto_update
 ):
     with test_client:
         create_response = test_client.post(
-            "drawing/new", json=jsonable_encoder(create_drawing_dto)
+            "drawing/new", json=jsonable_encoder(drawing_dto_create)
+        )
+        id_ = create_response.json()["id"]
+        drawing_dto_update.parent_id = id_ + 1
+
+        response = test_client.patch(
+            f"drawing/{id_}", json=jsonable_encoder(drawing_dto_update)
+        )
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_update_fails_already_exists(
+    test_client, drawings_dto_create, drawing_dto_update
+):
+    with test_client:
+        drawing_create_1, drawing_create_2 = drawings_dto_create(2)
+        test_client.post("drawing/new", json=jsonable_encoder(drawing_create_1))
+        test_client.post("drawing/new", json=jsonable_encoder(drawing_create_2))
+        drawing_dto_update.id = drawing_create_2.id
+
+        response = test_client.patch(
+            f"drawing/{drawing_create_1.id}", json=jsonable_encoder(drawing_dto_update)
+        )
+
+        assert response.status_code == status.HTTP_409_CONFLICT
+
+
+@pytest.mark.e2e
+def test_update_wrong_data_not_affect(
+    test_client, drawing_dto_create, drawing_dto_update
+):
+    with test_client:
+        create_response = test_client.post(
+            "drawing/new", json=jsonable_encoder(drawing_dto_create)
         )
         id_ = create_response.json()["id"]
 
         drawing_1_response = test_client.get(f"drawing/{id_}")
-        update_response = test_client.patch(f"drawing/{id_}", json={"wrong data": 0})
+        test_client.patch(f"drawing/{id_}", json={"wrong data": 0})
         drawing_2_response = test_client.get(f"drawing/{id_}")
 
-        assert update_response.status_code == status.HTTP_200_OK
         assert drawing_1_response.json() == drawing_2_response.json()
 
 
 @pytest.mark.e2e
-def test_delete_success(test_client, create_drawing_dto):
+def test_update_fails_validation(test_client, drawing_dto_create):
     with test_client:
         create_response = test_client.post(
-            "drawing/new", json=jsonable_encoder(create_drawing_dto)
+            "drawing/new", json=jsonable_encoder(drawing_dto_create)
+        )
+        id_ = create_response.json()["id"]
+
+        response = test_client.patch(f"drawing/{id_}", json={"wrong data": 0})
+
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.e2e
+def test_delete_success(test_client, drawing_dto_create):
+    with test_client:
+        create_response = test_client.post(
+            "drawing/new", json=jsonable_encoder(drawing_dto_create)
         )
         id_ = create_response.json()["id"]
 
         delete_response = test_client.delete(f"drawing/{id_}")
-
         get_response = test_client.get(f"drawing/{id_}")
 
         assert delete_response.status_code == status.HTTP_204_NO_CONTENT
