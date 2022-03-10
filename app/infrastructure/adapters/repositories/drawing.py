22efa88bae715ee5db95f6f177bb3570385c62fa
baalique <baalique@gmail.com>
2022-03-10 15:13:53 +0700

    ADD: update drawing services and repository to use with unit of work

--- app/infrastructure/adapters/repositories/drawing.py
+++ app/infrastructure/adapters/repositories/drawing.py
@@ -1,6 +1,5 @@
 from typing import Any, Dict, List, Optional
 
-from fastapi import Depends
 from sqlalchemy import delete, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -8,53 +7,44 @@ from app.domain.entities.drawing import Drawing
 from app.infrastructure.adapters.repositories.protocols.entities import (
     DrawingRepository,
 )
-from app.infrastructure.db.db import get_db
 
 
 class SQLAlchemyDrawingRepository(DrawingRepository):
-    def __init__(self, session: AsyncSession = Depends(get_db)):
+    def __init__(self, session: AsyncSession):
         self.session = session
 
     async def add(self, drawing: Drawing) -> Drawing:
-        async with self.session:
-            self.session.add(drawing)
-            await self.session.commit()
-            return drawing
+        self.session.add(drawing)
+        return drawing
 
     async def get(self, id: int) -> Optional[Drawing]:
-        async with self.session:
-            result = await self.session.execute(select(Drawing).where(Drawing.id == id))
-            drawing = result.scalar()
-            return drawing
+        result = await self.session.execute(select(Drawing).where(Drawing.id == id))
+        drawing = result.scalar()
+        return drawing
 
     async def list(self) -> List[Drawing]:
-        async with self.session:
-            result = await self.session.execute(select(Drawing).order_by(Drawing.id))
-            return result.scalars().all()
+        result = await self.session.execute(select(Drawing).order_by(Drawing.id))
+        return result.scalars().all()
 
     async def update(self, id: int, update_dict: Dict[str, Any]) -> Optional[Drawing]:
-        async with self.session:
-            _res = await self.session.execute(
-                update(Drawing)
-                .where(Drawing.id == id)
-                .values(**update_dict)
-                .returning(Drawing.id)
-            )
+        _res = await self.session.execute(
+            update(Drawing)
+            .where(Drawing.id == id)
+            .values(**update_dict)
+            .returning(Drawing.id)
+        )
 
-            result_id = _res.scalar()
-            await self.session.commit()
+        result_id = _res.scalar()
 
-            if result_id:
-                result = await self.get(result_id)
-                return result
-            else:
-                return None
+        if result_id:
+            result = await self.get(result_id)
+            return result
+        else:
+            return None
 
     async def delete(self, id: int) -> bool:
-        async with self.session:
-            _res = await self.session.execute(
-                delete(Drawing).where(Drawing.id == id).returning(Drawing.id)
-            )
-            result = _res.scalar()
-            await self.session.commit()
-            return bool(result)
+        _res = await self.session.execute(
+            delete(Drawing).where(Drawing.id == id).returning(Drawing.id)
+        )
+        result = _res.scalar()
+        return bool(result)
--- app/service_layer/services/drawing.py
+++ app/service_layer/services/drawing.py
@@ -1,8 +1,6 @@
 from typing import List, Optional
 
-from app.infrastructure.adapters.repositories.protocols.entities import (
-    DrawingRepository,
-)
+from app.infrastructure.protocols import UnitOfWork
 from app.service_layer.dtos.drawing import (
     DrawingDtoCreate,
     DrawingDtoOut,
@@ -12,73 +10,97 @@ from app.service_layer.exceptions import AlreadyExistsError, NotFoundError
 
 
 async def create_drawing(
-    drawing_create: DrawingDtoCreate, repo: DrawingRepository
+    drawing_create: DrawingDtoCreate, uow: UnitOfWork
 ) -> DrawingDtoOut:
-    exists = await id_exists(drawing_create.id, repo)
-    if exists:
-        raise AlreadyExistsError(f"Drawing with id {drawing_create.id} already exists")
+    async with uow:
+        exists = await id_exists(drawing_create.id, uow)
+        if exists:
+            raise AlreadyExistsError(
+                f"Drawing with id {drawing_create.id} already exists"
+            )
 
-    if drawing_create.parent_id is None:
-        parent = None
-    else:
-        parent = await repo.get(drawing_create.parent_id)
-        if not parent:
-            raise NotFoundError(f"Drawing with id {drawing_create.parent_id} not found")
+        if drawing_create.parent_id is None:
+            parent = None
+        else:
+            parent = await uow.drawings.get(drawing_create.parent_id)
+            if not parent:
+                raise NotFoundError(
+                    f"Drawing with id {drawing_create.parent_id} not found"
+                )
 
-    drawing = drawing_create.to_entity(parent)
-    result = await repo.add(drawing)
-    dto_out = DrawingDtoOut.from_entity(result)
-    return dto_out
+        drawing = drawing_create.to_entity(parent)
+        result = await uow.drawings.add(drawing)
+        dto_out = DrawingDtoOut.from_entity(result)
 
+        await uow.commit()
 
-async def get_drawing_by_id(id: int, repo: DrawingRepository) -> DrawingDtoOut:
-    result = await repo.get(id)
-    if not result:
-        raise NotFoundError(f"Drawing with id {id} not found")
-    return DrawingDtoOut.from_entity(result)
+        return dto_out
 
 
-async def get_all_drawings(repo: DrawingRepository) -> List[DrawingDtoOut]:
-    drawings = await repo.list()
-    return list(map(DrawingDtoOut.from_entity, drawings))
+async def get_drawing_by_id(id: int, uow: UnitOfWork) -> DrawingDtoOut:
+    async with uow:
+        result = await uow.drawings.get(id)
+        if not result:
+            raise NotFoundError(f"Drawing with id {id} not found")
+
+        return DrawingDtoOut.from_entity(result)
+
+
+async def get_all_drawings(uow: UnitOfWork) -> List[DrawingDtoOut]:
+    async with uow:
+        drawings = await uow.drawings.list()
+
+        return list(map(DrawingDtoOut.from_entity, drawings))
 
 
 async def update_drawing(
-    id: int, drawing_update: DrawingDtoUpdate, repo: DrawingRepository
+    id: int, drawing_update: DrawingDtoUpdate, uow: UnitOfWork
 ) -> Optional[DrawingDtoOut]:
-    exists = await id_exists(id, repo)
-    if not exists:
-        raise NotFoundError(f"Drawing with id {drawing_update.parent_id} not found")
-    exists = await id_exists(drawing_update.id, repo)
-    if exists and id != drawing_update.id:
-        raise AlreadyExistsError(f"Drawing with id {drawing_update.id} already exists")
-    exists = await id_exists(drawing_update.parent_id, repo)
-    if drawing_update.parent_id is not None and not exists:
-        raise NotFoundError(f"Drawing with id {drawing_update.parent_id} not found")
+    async with uow:
+        exists = await id_exists(id, uow)
+        if not exists:
+            raise NotFoundError(f"Drawing with id {drawing_update.parent_id} not found")
+        exists = await id_exists(drawing_update.id, uow)
+        if exists and id != drawing_update.id:
+            raise AlreadyExistsError(
+                f"Drawing with id {drawing_update.id} already exists"
+            )
+        exists = await id_exists(drawing_update.parent_id, uow)
+        if drawing_update.parent_id is not None and not exists:
+            raise NotFoundError(f"Drawing with id {drawing_update.parent_id} not found")
 
-    old_drawing = await get_drawing_by_id(id, repo)
-    update_dict = old_drawing.dict() | {
-        k: v
-        for k, v in drawing_update.dict().items()
-        if v is not None or k == "parent_id"
-    }
+        old_drawing = await get_drawing_by_id(id, uow)
+        update_dict = old_drawing.dict() | {
+            k: v
+            for k, v in drawing_update.dict().items()
+            if v is not None or k == "parent_id"
+        }
 
-    drawing = await repo.update(id, update_dict=update_dict)
+        drawing = await uow.drawings.update(id, update_dict=update_dict)
 
-    return DrawingDtoOut.from_entity(drawing) if drawing else None
+        await uow.commit()
 
-
-async def delete_drawing(id: int, repo: DrawingRepository) -> bool:
-    exists = await id_exists(id, repo)
-    if not exists:
-        raise NotFoundError(f"Drawing with id {id} not found")
-
-    result = await repo.delete(id)
-    return result
+        return DrawingDtoOut.from_entity(drawing) if drawing else None
 
 
-async def id_exists(id: Optional[int], repo: DrawingRepository) -> bool:
-    if id is None:
-        return False
-    all_drawings = await get_all_drawings(repo)
-    return id in (d.id for d in all_drawings)
+async def delete_drawing(id: int, uow: UnitOfWork) -> bool:
+    async with uow:
+        exists = await id_exists(id, uow)
+        if not exists:
+            raise NotFoundError(f"Drawing with id {id} not found")
+
+        result = await uow.drawings.delete(id)
+
+        await uow.commit()
+
+        return result
+
+
+async def id_exists(id: Optional[int], uow: UnitOfWork) -> bool:
+    async with uow:
+        if id is None:
+            return False
+
+        all_drawings = await get_all_drawings(uow)
+
+        return id in (d.id for d in all_drawings)
